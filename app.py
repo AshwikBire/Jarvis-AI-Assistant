9f512975d05194944d80fdfe8ff068d36b341746
Ashwik Rajendra Bire <94124884+AshwikBire@users.noreply.github.com>
2025-07-28 13:48:58 +0530

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GEMINI_API_KEY = os.getenv("AIzaSyC48094RjAvbcPdUfLdltouGvP7LB6cHwk")
 
 st.set_page_config(page_title="Jarvis AI Assistant", page_icon="🤖", layout="wide")
 st.title("🤖 Jarvis AI Assistant")
